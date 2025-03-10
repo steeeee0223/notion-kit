da90fa2e3aeacf2e441fde4a6ffa659c085a0de0
shihwen.yu <shihwen.yu@tronfuture.com>
2025-03-10 18:11:11 +0800

    wip: docs

---
+++ apps/docs/src/components/docs/component-preview.tsx
@@ -0,0 +1,104 @@
+import React from "react";
+import { Index } from "@/__registry__/demos";
+import { CodeBlock } from "@/components/docs/code-block";
+import { getFileSource } from "@/lib/get-file-source";
+import { CurrentThemeProvider } from "@/modules/themes/components/current-theme-provider";
+import { ScrollArea } from "@radix-ui/react-scroll-area";
+import { SlidersHorizontalIcon } from "lucide-react";
+
+import { cn } from "@notion-kit/cn";
+import { Button } from "@notion-kit/shadcn";
+
+import {
+  ComponentWrapper,
+  Loader,
+  ResizableContainer,
+} from "./component-preview-client";
+
+export interface ComponentPreviewProps {
+  name: string;
+  containerClassName?: string;
+  className?: string;
+  preview?: string;
+  expandable?: boolean;
+  fullWidth?: boolean;
+  resizable?: boolean;
+  suspense?: boolean;
+  primary?: boolean;
+}
+
+export const ComponentPreview = async ({
+  name,
+  containerClassName,
+  preview,
+  expandable = true,
+  fullWidth = false,
+  resizable = false,
+  suspense = false,
+  primary = false,
+  // eslint-disable-next-line @typescript-eslint/require-await
+}: ComponentPreviewProps) => {
+  const demoItem = Index[name];
+
+  // @ts-expect-error TODO fix later
+  const Component = demoItem.component;
+  // @ts-expect-error TODO fix later
+  const code: { fileName: string; code: string }[] = demoItem.files.map(
+    (file: string) => {
+      const { fileName, content } = getFileSource(file);
+      return {
+        fileName,
+        code: content,
+      };
+    },
+  );
+
+  return (
+    <div
+      className={cn("overflow-hidden rounded-md border", containerClassName)}
+    >
+      <div className="bg-bg-muted">
+        <ResizableContainer resizable={resizable}>
+          <Loader>
+            <CurrentThemeProvider className="relative duration-300">
+              <Button size="icon-sm" className="absolute top-2 right-2 z-10">
+                <SlidersHorizontalIcon />
+              </Button>
+              <ScrollArea className="bg-bg text-fg">
+                <div
+                  className={cn(
+                    "flex py-10",
+                    primary && "min-h-48 py-20",
+                    fullWidth
+                      ? "px-8 lg:px-12"
+                      : "flex items-center justify-center px-4",
+                  )}
+                >
+                  <div
+                    className={cn(
+                      fullWidth ? "w-full" : "flex items-center justify-center",
+                    )}
+                  >
+                    <ComponentWrapper suspense={suspense}>
+                      <Component />
+                    </ComponentWrapper>
+                  </div>
+                </div>
+              </ScrollArea>
+            </CurrentThemeProvider>
+          </Loader>
+        </ResizableContainer>
+      </div>
+      <CodeBlock
+        files={code.map((file) => ({
+          fileName: file.fileName,
+          code: file.code,
+          lang: "tsx",
+        }))}
+        preview={preview}
+        className={"w-full rounded-t-none border-x-0 border-b-0"}
+        expandable={expandable}
+      />
+    </div>
+  );
+};
---
+++ apps/docs/src/hooks/use-mounted.ts
@@ -0,0 +1,12 @@
+import React from "react";
+
+export function useMounted() {
+  const [isMounted, setIsMounted] = React.useState(false);
+
+  React.useEffect(() => {
+    setIsMounted(true);
+    return () => setIsMounted(false);
+  }, []);
+
+  return isMounted;
+}
