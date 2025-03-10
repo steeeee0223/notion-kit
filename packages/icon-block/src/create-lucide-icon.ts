0cd714948b3799948100195b1956d3af4eab67db
steeeee0223 <mr.steven7@gmail.com>
2025-03-10 13:49:52 +0000

    wip: shared registry

---
+++ packages/icon-block/src/create-lucide-icon.ts
@@ -0,0 +1,31 @@
+import type { IconNode, LucideProps } from "lucide-react";
+import { createElement, forwardRef } from "react";
+import { Icon } from "lucide-react";
+
+import { cn } from "@notion-kit/cn";
+
+import type { LucideName } from "./types";
+import { iconNodes } from "./data";
+import { toPascalCase } from "./utils";
+
+/**
+ * Create a Lucide icon component
+ */
+export const createLucideIcon = (name: LucideName) => {
+  const nodeWithKeys = iconNodes[name].map(([elem, attrs], i) => [
+    elem,
+    { ...attrs, key: `${name}-${i}` },
+  ]) as IconNode;
+  const Component = forwardRef<SVGSVGElement, LucideProps>(
+    ({ className, ...props }, ref) =>
+      createElement(Icon, {
+        ref,
+        iconNode: nodeWithKeys,
+        className: cn(`lucide-${name}`, className),
+        ...props,
+      }),
+  );
+
+  Component.displayName = toPascalCase(name);
+  return Component;
+};
---
+++ packages/icon-block/src/icon-block.tsx
@@ -0,0 +1,93 @@
+"use client";
+
+import type { VariantProps } from "class-variance-authority";
+import React from "react";
+import { cva } from "class-variance-authority";
+
+import { cn } from "@notion-kit/cn";
+import { Avatar, AvatarFallback, AvatarImage } from "@notion-kit/shadcn";
+import { Spinner } from "@notion-kit/spinner";
+
+import type { IconInfo } from "./types";
+import { createLucideIcon } from "./create-lucide-icon";
+import { getLetter, isEmoji } from "./utils";
+
+const iconBlockVariants = cva("shrink-0", {
+  variants: {
+    size: {
+      sm: "size-5 rounded-sm p-0.5 text-sm/4",
+      md: "size-10 rounded-md p-1 text-3xl/8",
+      lg: "size-16 rounded-md p-1 text-5xl/tight",
+      xl: "size-[78px] rounded-lg p-1 text-7xl",
+    },
+  },
+  defaultVariants: { size: "sm" },
+});
+
+export interface IconBlockProps extends VariantProps<typeof iconBlockVariants> {
+  className?: string;
+  icon: IconInfo;
+  fallback?: string;
+}
+
+/**
+ * A block for icon display.
+ */
+export const IconBlock: React.FC<IconBlockProps> = ({
+  className,
+  size,
+  ...props
+}) => {
+  return (
+    <Icon {...props} className={cn(iconBlockVariants({ size, className }))} />
+  );
+};
+
+const Icon: React.FC<Omit<IconBlockProps, "size">> = ({
+  className,
+  icon,
+  fallback = " ",
+}) => {
+  switch (icon.type) {
+    case "lucide": {
+      const Icon = createLucideIcon(icon.name);
+      return (
+        <Icon color={icon.color} className={className} aria-label={icon.name} />
+      );
+    }
+    case "file":
+      return (
+        <Avatar className={className}>
+          <AvatarFallback className="bg-transparent">
+            <Spinner className={className} />
+          </AvatarFallback>
+          <AvatarImage src={icon.url} alt="icon" />
+        </Avatar>
+      );
+    default: {
+      if (icon.type === "emoji" && isEmoji(icon.emoji)) {
+        return (
+          <div
+            className={cn(
+              "text-primary dark:text-primary/80 text-center",
+              className,
+            )}
+          >
+            {icon.emoji}
+          </div>
+        );
+      }
+      const letter = getLetter(icon, fallback);
+      return (
+        <div
+          className={cn(
+            "bg-primary/10 text-secondary dark:text-secondary-dark text-center",
+            className,
+          )}
+        >
+          {letter}
+        </div>
+      );
+    }
+  }
+};
