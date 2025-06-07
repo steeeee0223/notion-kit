455b50933ef477161c18f19e52470ea8368c7240
steeeee0223 <mr.steven7@gmail.com>
2025-06-07 16:26:15 +0800

    init: auth, auth-server

---
+++ apps/auth-server/src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { ReactNode } from "react";
+import type { Metadata } from "next";
+import { IBM_Plex_Mono } from "next/font/google";
+
+import "./global.css";
+
+import { cn } from "@notion-kit/cn";
+import { Toaster } from "@notion-kit/shadcn";
+
+export const ibm_plex_mono = IBM_Plex_Mono({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  display: "swap",
+  variable: "--font-ibm-plex-mono",
+});
+
+export const metadata: Metadata = {
+  title: "Notion Auth Server",
+  icons: "/logo.svg",
+};
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <html
+      lang="en"
+      className={cn(ibm_plex_mono.className, ibm_plex_mono.variable)}
+      suppressHydrationWarning
+    >
+      <body className="flex min-h-screen flex-col">
+        {children}
+        <Toaster className="font-mono" />
+      </body>
+    </html>
+  );
+}
---
+++ apps/auth-server/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <main className="flex h-screen w-screen items-center justify-center text-primary">
+      Home
+    </main>
+  );
+}
