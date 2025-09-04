cf7be720268c3ea14d6581971565440d106d29ea
steeeee0223 <mr.steven7@gmail.com>
2025-09-04 17:57:26 +0800

    update workspace home page

--- examples/notion-clone/src/app/(protected)/(core)/workspace/[slug]/page.tsx
+++ examples/notion-clone/src/app/(protected)/(core)/workspace/[slug]/page.tsx
@@ -1,14 +1,22 @@
 "use client";
 
+import { IconBlock } from "@notion-kit/icon-block";
+
 import { useSettings } from "@/hooks/use-settings";
 
 export default function Page() {
   const { settings } = useSettings();
 
   return (
-    <main className="flex h-screen w-full flex-col items-center justify-center gap-6">
-      <div>Hi, {settings.account.preferredName}!</div>
-      <div>Welcome to {settings.workspace.name}</div>
+    <main className="flex h-screen w-full flex-col items-center">
+      <div className="mt-10 flex h-20 flex-col items-center justify-center text-2xl">
+        <span>Hi, {settings.account.preferredName}!</span>
+        <div className="flex items-center gap-2">
+          Welcome to
+          <IconBlock icon={settings.workspace.icon} size="md" />
+          {settings.workspace.name}
+        </div>
+      </div>
     </main>
   );
 }
