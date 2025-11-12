ead9224c71065d2b6270fe71e204eed9e056697f
steeeee0223 <mr.steven7@gmail.com>
2025-11-12 17:03:26 +0800

    fix config

--- packages/cn/tsup.config.ts
+++ packages/cn/tsup.config.ts
@@ -1,11 +1,30 @@
 import { defineConfig } from "tsup";
 
+const silent = false;
+
 export default defineConfig((opts) => {
   return [
     {
       ...opts,
-      config: "@notion-kit/config/tsup",
-      external: undefined,
+      clean: true,
+      dts: true,
+      format: ["cjs", "esm"],
+      sourcemap: true,
+      splitting: false,
+      ...(silent
+        ? {
+            silent: true,
+            onSuccess: async () => {
+              if (opts.watch) {
+                console.info("Watching for changes...");
+
+                return;
+              }
+
+              console.info("Build succeeded!");
+            },
+          }
+        : {}),
       entry: ["src/index.ts"],
     },
   ];
