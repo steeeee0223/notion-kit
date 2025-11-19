fcfac37241b219fe916cb4f72d71bf8a94e4d47f
steeeee0223 <mr.steven7@gmail.com>
2025-11-19 21:26:06 +0800

    remove use-mounted

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -4,7 +4,6 @@ export * from "./use-filter";
 export * from "./use-input-field";
 export * from "./use-is-client";
 export * from "./use-is-mobile";
-export * from "./use-mounted";
 export * from "./use-origin";
 export * from "./use-rect";
 export * from "./use-scroll";
