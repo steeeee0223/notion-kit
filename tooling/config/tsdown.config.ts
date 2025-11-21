940f5365d0c9cb767e10a2aa8c449cae5864d3d7
Shih-Wen Yu <79098634+steeeee0223@users.noreply.github.com>
2025-11-21 10:06:50 +0800

    tsup → tsdown (#79)

---
+++ tooling/config/tsdown.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "tsdown";
+
+export default defineConfig((opts) => {
+  return [
+    {
+      ...opts,
+      dts: true,
+      sourcemap: true,
+      external: [/@notion-kit\/.+/],
+      logLevel: "warn",
+      onSuccess: async () => {
+        if (opts.watch) {
+          console.info("Watching for changes...");
+          return;
+        }
+        console.info("Build successfully!");
+      },
+      entry: ["src/index.ts"],
+    },
+  ];
+});
