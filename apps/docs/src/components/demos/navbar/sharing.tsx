19037716294bb80d9949d601abbbffdc3b1bdf9b
steeeee0223 <mr.steven7@gmail.com>
2026-01-19 10:08:10 +0800

    update imports

--- apps/docs/src/components/demos/navbar/sharing.tsx
+++ apps/docs/src/components/demos/navbar/sharing.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 
-import { Publish } from "@notion-kit/navbar";
+import { Publish } from "@notion-kit/navbar/presets";
 import { Page } from "@notion-kit/schemas";
 import { TooltipProvider } from "@notion-kit/shadcn";
 
