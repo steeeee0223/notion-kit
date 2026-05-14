da0aa4f287fec0bf922e768795e6e52943107539
steeeee0223 <mr.steven7@gmail.com>
2026-05-14 14:04:36 +0800

    move generate slug to org-extra

--- packages/auth-ui/src/auth-provider/auth-provider.tsx
+++ packages/auth-ui/src/auth-provider/auth-provider.tsx
@@ -1,16 +1,10 @@
-"use client";
-
 import React, { createContext, use, useMemo } from "react";
-import { v4 } from "uuid";
 
 import { createAuthClient, type AuthClient } from "@notion-kit/auth";
-
-import { toSlugLike } from "../lib";
 
 interface AuthContextInterface {
   auth: AuthClient;
   appURL: string;
-  generateUniqueSlug: (name: string) => Promise<string>;
   redirect?: (url: string) => void;
 }
 
@@ -70,22 +64,6 @@ function AuthProvider({
     return {
       auth,
       appURL: appBaseURL,
-      generateUniqueSlug: async (name) => {
-        const baseSlug = toSlugLike(name);
-        let slug = baseSlug;
-        let counter = 0;
-
-        while (counter < 10) {
-          const res = await auth.organization.checkSlug({ slug });
-          if (!res.error) break;
-          const id = v4();
-          slug = `${baseSlug}-${id.slice(0, 8)}`;
-          counter++;
-        }
-
-        // The slug is guaranteed to be unique at this point if counter < 10
-        return slug;
-      },
       redirect: (url) => {
         const resolvedURL = resolveAppURL(appBaseURL, url);
         if (redirect) {
