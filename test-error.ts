fa9240db3fa8d4b218b877ef2fcabcb3f34c15d5
steeeee0223 <mr.steven7@gmail.com>
2026-05-28 17:21:31 +0800

    clean repo

--- test-error.ts
+++
@@ -1,7 +0,0 @@
-import { notFound } from "./apps/map-server/src/lib/api-error";
-try {
-  throw notFound("Test");
-} catch (error) {
-  console.log("statusCode in error:", error && typeof error === "object" && "statusCode" in error);
-  console.log("error.statusCode:", (error as any).statusCode);
-}
