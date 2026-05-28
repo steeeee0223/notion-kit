0a6a3323721ca99cb172f2039507186606313153
steeeee0223 <mr.steven7@gmail.com>
2026-05-28 17:12:50 +0800

    fix: typecheck, format

--- apps/e2e/vitest.config.ts
+++ apps/e2e/vitest.config.ts
@@ -1,8 +1,9 @@
 import react from "@vitejs/plugin-react";
+import type { PluginOption } from "vite";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react() as PluginOption],
   test: {
     name: "e2e",
     environment: "jsdom",
--- packages/ui/vitest.config.ts
+++ packages/ui/vitest.config.ts
@@ -1,9 +1,10 @@
 import path from "node:path";
 import react from "@vitejs/plugin-react";
+import type { PluginOption } from "vite";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react() as PluginOption],
   test: {
     name: "ui",
     environment: "jsdom",
